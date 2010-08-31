f82cc599ed01d43741f85a28266a44ab13db3cff
Chris Beer <chris@authoritativeopinion.com>
2010-08-31 14:39:28 -0400

    add thumbnail sized image for mosaic

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,6 @@
 class Station < ActiveRecord::Base
   acts_as_taggable_on :tags, :affiliation, :color
-  has_attached_file :home
+  has_attached_file :home, :styles => { :thumb => ["100x100#", :png] }
   acts_as_commentable
   acts_as_rateable
 end
