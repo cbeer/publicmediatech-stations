2522cf2378b62f2ec9afc8c77cae2ae8a5475f3d
Chris Beer <chris@authoritativeopinion.com>
2010-09-04 14:59:48 -0400

    add nokogiri xml library to think about parsing content..

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
  config.gem "acts-as-taggable-on", :source => "http://gemcutter.org", :version => '2.0.0.rc1'
 config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 require 'RMagick'
+config.gem 'nokogiri'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
