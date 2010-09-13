0b534e25d86c89fb822b7062184263bc405804ae
Chris Beer <chris@authoritativeopinion.com>
2010-09-12 20:30:36 -0400

    add new derivative size..

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,7 +1,7 @@
 class Website < ActiveRecord::Base
   belongs_to :station
   acts_as_taggable_on :tags, :color_palette
-  has_attached_file :screenshot, :styles => { :thumb => ["100x100#", :png] }
+  has_attached_file :screenshot, :styles => { :thumb => ["100x100#", :png], :gallery => ["480x360#", :png] }
   acts_as_commentable
   acts_as_rateable
 end
